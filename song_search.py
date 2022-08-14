5e3bd8d714c85073167c17806acaf93ca7586dec
zarossa <mr.kirarik@yandex.ru>
2022-08-15 02:24:07 +0300

    launching the search engine

--- song_search.py
+++ song_search.py
@@ -6,21 +6,35 @@ from lxml import etree
 def parse(url_address):
     api = requests.get(url_address).text
     tree = lxml.html.fromstring(api)
-    songlist = tree.xpath('//tr/td[@class="artist_name"]')
-    for i in range(5):
-        print(songlist[i].text_content(), songlist[i])
+    return tree.xpath('//tr/td[@class="artist_name"]')
 
 
 
-
-
-
-url = 'https://amdm.ru/search/?q=пачка+сигарет'
+def url_maker():
+    text = input().split()
+    return 'https://amdm.ru/search/?q=' + '+'.join(text)
 
 
 def main():
-    return parse(url)
+    return parse(url_maker())
 
-
-if __name__ == '__main__':
-    main()
+songlist = main()
+k = 0
+while True:
+    for i in range(k * 5, (k + 1) * 5):
+        print(i + 1, songlist[i].text_content(), songlist[i][1].attrib['href'])
+    if k != 3:
+        if input('Next?(y/n)\n') == 'y':
+            k += 1
+        else:
+            break
+    else:
+        break
+# k = 0
+# while True:
+#     if __name__ == '__main__':
+#         main()
+#     if input('More?\n') == 'y':
+#         k += 1
+#     else:
+#         break
